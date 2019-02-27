00ea4fbc21fb9cf5d67fe30b79211e398bc9806a
Peter Borovansky <peter.borovansky@microstep-mis.com>
2019-02-27 15:27:51 +0100

    druhe cvicenie

--- CV02/Priprava_CV02.hs
+++ CV02/Priprava_CV02.hs
@@ -1,7 +1,7 @@
 module Priprava_CV02 where
 import Test.QuickCheck
 import Text.Show.Functions
-import Data.List(sort)
+import Data.List(sort,nub)
 
 -- definujte predikat pre usporiadany/rastuci/nerastuci zoznam
 ordered :: [Int] -> Bool
@@ -9,10 +9,18 @@ ordered [] = True
 ordered [_] = True
 ordered (x:xs@(y:ys)) = x<=y && ordered xs
 
+ordered' :: [Int] -> Bool
+ordered' [] = True
+ordered'  xs = all (\(x,y) -> x <= y) $ zip (init xs) (tail xs)
+
+
 -- definujte a overte nejaku vlastnost funkcie ordered
 
-qch1 = undefined -- quickCheck( \xs -> cond ==> proposition )
-qch1v = undefined -- verboseCheck( \xs -> cond ==> proposition )
+qch1 = quickCheck( \xs -> ordered xs == ordered' xs )
+qch1v = verboseCheck(\xs -> ordered $ sort xs  )
+qch1v' = verboseCheck(\xs -> ordered' $ sort xs  )
+
+qch1r = verboseCheck(\xs -> (length xs > 1) ==> not $ ordered $ reverse $ sort xs  )
 
 -- naprogramuje jeden prechod bubble sort algoritmu
 bubble :: [Int] -> [Int]
@@ -27,8 +35,29 @@ e = bubbleSort [4,3,4,6,7,4,3,1,1,2,3,4,5,6,7,8,9,0,5,3,2,3,2,3,4,5,6,7,1,2,2,0,
 --[0,0,1,1,1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,5,5,5,6,6,6,7,7,7,8,9,9,11,12]
 
 -- definujte a overte nejaku vlastnost funkcie bubbleSort
-qch2 = undefined -- quickCheck( \xs -> cond ==> proposition )
+qch2 = quickCheck( \xs -> bubbleSort xs == sort xs)
 qch2v = undefined -- verboseCheck( \xs -> cond ==> proposition )
+
+powerSet :: [t] -> [[t]]
+powerSet [] = [[]]
+powerSet (x:xs) = let ps = powerSet xs in ps ++ [x:ys | ys <- ps]
+
+qch3 = quickCheck((\xs -> (length xs < 10) ==> (length $ powerSet xs) == (2^(length xs)))::[Int]->Property)
+
+qch3' = verboseCheck((\xs -> (length xs < 10) ==> 
+		(length $ nub $ powerSet xs) == (2^(length xs)))::[Int]->Property)
+
+qch3'' = verboseCheck((\n -> (n >= 0 && n < 10) ==> 
+		(length $ nub $ powerSet [1..n]) == (2^n))::Int->Property)
+
+subset :: (Eq t) => [t] -> [t] -> Bool
+subset xs ys = and [ elem x ys | x<-xs]
+
+subset' :: (Eq t) => [t] -> [t] -> Bool
+subset' xs ys = all  (\x -> elem x ys) xs
+
+qch3''' = quickCheck((\n -> (n >= 0 && n < 10) ==> 
+					    and [ subset m [1..n] | m <- powerSet [1..n] ])::Int->Property)
 
 ---------------------------------
 -- kompozícia zoznamu funkcií,  :: [a->a] -> (a->a)
@@ -42,6 +71,9 @@ kompozicia (f:fs) = (\x -> f (kompozicia fs x))
 kompozicia''  :: [a->a] -> (a->a)
 kompozicia'' [] = id
 kompozicia'' (f:fs) = \x -> f $ kompozicia'' fs x
+
+qchf = quickCheck((\f -> \g -> \x -> (kompozicia [f,g]) x == (kompozicia'' [f,g]) x)::
+			(Int->Int) -> (Int->Int) -> Int -> Bool)
 
 -- definicia haskellistu, co si este prehodil x na lavu stranu
 kompozicia'''''  :: [a->a] -> (a->a)
--- CV02/Streda_CV02.hs
+++ CV02/Streda_CV02.hs
@@ -5,21 +5,37 @@ import Data.List(sort)
 
 -- definujte predikat pre usporiadany/rastuci/nerastuci zoznam
 ordered :: [Int] -> Bool
-ordered = undefined
+ordered [] = True
+ordered [_] = True
+ordered (x:y:xs) = x<=y && ordered (y:xs) 
+
+ordered' [] = True
+ordered' xs = all (\(p,d)->p<=d) $zip (init xs)(tail xs)
+
+qch3 = quickCheck(\xs -> ordered xs == ordered' xs)
 
 -- definujte a overte nejaku vlastnost funkcie ordered
-qch1 = undefined -- quickCheck( \xs -> cond ==> proposition )
+qch1 = quickCheck( \xs -> ordered xs ==(xs==sort xs) ) -- quickCheck( \xs -> cond ==> proposition )
+qch1' = quickCheck( \xs -> ordered (sort xs) )
 qch1v = undefined -- verboseCheck( \xs -> cond ==> proposition )
 
 -- naprogramuje jeden prechod bubble sort algoritmu
 bubble :: [Int] -> [Int]
-bubble = undefined
+bubble [] = [] 
+bubble [x] = [x] 
+bubble (x:y:xs) = if x>y then y:bubble (x:xs) else x:bubble (y:xs)
+
+qch4 = quickCheck( \xs -> (xs/=[]) ==>(maximum xs) == last (bubble xs) )
+qch5 = quickCheck( \xs -> length (xs) == length (bubble xs) )
 
 -- pouzite bubble na bubbleSort
 bubbleSort  :: [Int]->[Int]
-bubbleSort xs = undefined
+bubbleSort xs = (iterate bubble xs)!!(length xs)
 e = bubbleSort [4,3,4,6,7,4,3,1,1,2,3,4,5,6,7,8,9,0,5,3,2,3,2,3,4,5,6,7,1,2,2,0,9,12,11]
 --[0,0,1,1,1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,5,5,5,6,6,6,7,7,7,8,9,9,11,12]
+
+qch6 = quickCheck( \xs -> ordered $bubbleSort xs )
+qch7 = quickCheck( \xs -> bubbleSort xs == sort xs )
 
 -- definujte a overte nejaku vlastnost funkcie bubbleSort
 qch2 = undefined -- quickCheck( \xs -> cond ==> proposition )
@@ -30,7 +46,12 @@ qch2v = undefined -- verboseCheck( \xs -> cond ==> proposition )
 
 -- zaciatocnicka definicia cez zoznamovu rekurziu
 kompozicia  :: [a->a] -> (a->a)
-kompozicia = undefined
+kompozicia [] = id
+kompozicia [f] = f
+--kompozicia (f:fs) = f . (kompozicia fs)
+--kompozicia (f:fs) = \x -> f $kompozicia fs x 
+kompozicia xs = foldr (.) id xs 
+ 
 
 -- definicia haskellistu, co si nasiel operator $
 kompozicia''  :: [a->a] -> (a->a)
